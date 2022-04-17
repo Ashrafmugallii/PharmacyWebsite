5df398ebf9645e184712381e5e5980446b98ed8c
Ashrafmugallii <aam282@njit.edu>
2022-04-17 16:55:38 -0400

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$('.veccine').slick({
+    arrows: false,
+    dots: true,
+});
+$('.banners').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false,
+    autoplay: true
+  });
